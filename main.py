a9d71922e4d18d216e2a923e42ce08df6eda7d4e
Wumpie7 <chrisjaver@gmail.com>
2023-03-26 23:03:20 +0200

    Added sources

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
+#Assignment: https://dlo.mijnhva.nl/d2l/lms/dropbox/user/folder_submit_files.d2l?db=207186&grpid=0&isprv=0&bp=0&ou=471098
+#Assignment Guide: file:///C:/Users/Christan/Desktop/Big-Data-Engineer/Big%20Data%20Scientist%20and%20Engineer%20Individual%20Assignment%201-old%20program.pdf
 print("Hello World!")
