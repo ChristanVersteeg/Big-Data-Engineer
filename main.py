b94d1f05edbc9a497fbca2163e9c3edfb85ea13f
Wumpie7 <christan.versteeg@hva.nl>
2023-04-12 18:15:58 +0200

    The Engineer, is Engi-here heheh

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #Assignment: https://dlo.mijnhva.nl/d2l/lms/dropbox/user/folder_submit_files.d2l?db=207186&grpid=0&isprv=0&bp=0&ou=471098
 #Assignment Guide: file:///C:/Users/Christan/Desktop/Big-Data-Engineer/Big%20Data%20Scientist%20and%20Engineer%20Individual%20Assignment%201-old%20program.pdf
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from textblob import TextBlob
+import time
+import pandas as pd
 import csv
+import random
 
 # Load the CSV file into a pandas dataframe
 df = pd.read_csv("Hotel_Reviews.csv")
@@ -11,45 +14,51 @@ df = pd.read_csv("Hotel_Reviews.csv")
 # View the first few rows of the dataframe
 print(df.head())
 
-# URL of the page you want to scrape
-url = "https://www.tripadvisor.com/Hotels-g187147-Paris_Ile_de_France-Hotels.html"
+# Replace this URL with the one you want to scrape reviews from
+url = 'https://www.tripadvisor.com/Hotels-g187147-Paris_Ile_de_France-Hotels.html'
 
-print(f"Scraping {url}...")
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
 
-# Make a GET request to the URL
-response = requests.get(url)
+def get_review_data(url):
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching the URL: {e}")
+        return []
 
-# Check if the request was successful
-if response.status_code == 200:
-    print("Request successful.")
+    soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.content, "html.parser")
-    print("HTML content parsed.")
+    # Replace the CSS selector with the appropriate one for the website you're scraping
+    reviews = soup.select('.ghulh')
     
-    # Find the elements you want to extract
-    hotels = soup.find_all("div", class_="ui_column is-8 main_col allowEllipsis")
-    print(f"Found {len(hotels)} hotels.")
+    return [review.get_text() for review in reviews]
+
+def classify_review_sentiment(review_text):
+    analysis = TextBlob(review_text)
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity < 0:
+        return 'negative'
+    else:
+        return 'neutral'
+
+def main():
+    review_texts = get_review_data(url)
     
-    # Create a CSV file to store the extracted data
-    with open("tripadvisor_hotels.csv", "w", encoding="utf-8", newline="") as csvfile:
-        writer = csv.writer(csvfile)
+    if not review_texts:
+        print("No reviews found.")
+        return
 
-        # Write the header row to the CSV file
-        writer.writerow(["Hotel Name", "Location", "Price", "Rating"])
-        print("Header row written.")
+    classified_reviews = [(review, classify_review_sentiment(review)) for review in review_texts]
 
-        # Loop through the hotels and extract the relevant information
-        for hotel in hotels:
-            name = hotel.find("a", class_="_15_ydu6b").get_text().strip()
-            location = hotel.find("span", class_="_21qUqkJx").get_text().strip()
-            price = hotel.find("div", class_="_3hDPbqJU _3hmsP_6p").get_text().strip()
-            rating = hotel.find("span", class_="ui_bubble_rating").get("alt").split()[0]
+    for review, sentiment in classified_reviews:
+        print(f"Review: {review.strip()}\nSentiment: {sentiment}\n")
 
-            # Write the extracted information to the CSV file
-            writer.writerow([name, location, price, rating])
-            print(f"Wrote hotel data: {name}, {location}, {price}, {rating}")
-    
-    print("Data extraction complete.")
-else:
-    print(f"Request failed with status code {response.status_code}.")
+    # Introduce a delay between requests to avoid overwhelming the server or getting blocked
+    time.sleep(random.uniform(1, 3))
+
+if __name__ == '__main__':
+    main()
