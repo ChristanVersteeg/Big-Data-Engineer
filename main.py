da30f8a030cd05293848188c291f7f6253caac0b
Wumpie7 <chrisjaver@gmail.com>
2024-04-04 05:26:36 +0200

    Added VADER

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #Assignment: https://dlo.mijnhva.nl/d2l/lms/dropbox/user/folder_submit_files.d2l?db=249019&grpid=0&isprv=0&bp=0&ou=540323
 #Assignment Guide: file:///C:/Users/Christan/Desktop/Big-Data-Engineer/Big%20Data%20Scientist%20and%20Engineer%20Individual%20Assignment%201-old%20program.pdf
-from textblob import TextBlob
 import time
 import pandas as pd
 from enum import Enum
@@ -10,6 +9,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
 
 import sys #Set the encoding for standard output to UTF-8, to prevent UnicodeEncodeError(s)
 if not sys.stdout.encoding or sys.stdout.encoding.lower() != 'utf-8': sys.stdout.reconfigure(encoding='utf-8')
@@ -32,7 +35,8 @@ def setup_custom_reviews():
                 "This hotel was a damn mess. The bed sheets weren't made, the food was horrid and the staff wasn't helpful nor nice. I would not recommend.", 
                 "This is your everyday average hotel, not bad, not good, reasonably priced. All in all, rather happy with how it turned out", 
                 "Man this hotel was so damn amazing, and it wasn't even that expensive! It's surprising how nice the staff is, how clean the rooms are, and above all, how tasty the damn food is. I would highly recommend anyone in the proximity to go check this hotel out!"],
-            'Sentiment': [Sentiment.NULL, Sentiment.NULL, Sentiment.NULL]}
+            'TextBlob_Sentiment': [Sentiment.NULL, Sentiment.NULL, Sentiment.NULL],
+            'VADER_Sentiment': [Sentiment.NULL, Sentiment.NULL, Sentiment.NULL]}
     
     return custom_reviews
 
@@ -45,7 +49,9 @@ def setup_kaggle_reviews():
     kaggle_reviews = {
             Type: [Type.KAGGLE] * num_reviews,
             'Review': kaggle_concat,
-            'Sentiment': [Sentiment.NULL] * num_reviews}
+            'TextBlob_Sentiment': [Sentiment.NULL] * num_reviews,
+            'VADER_Sentiment': [Sentiment.NULL] * num_reviews
+              }
     
     return kaggle_reviews
 
@@ -68,7 +74,8 @@ def setup_scraped_reviews():
     scraped_reviews = {
         Type: [Type.SCRAPED] * num_reviews,
         'Review': scraped_review_texts,
-        'Sentiment': [Sentiment.NULL] * num_reviews
+        'TextBlob_Sentiment': [Sentiment.NULL] * num_reviews,
+        'VADER_Sentiment': [Sentiment.NULL] * num_reviews
     }
 
     chrome.quit()
@@ -81,15 +88,28 @@ all_reviews = pd.concat([
     pd.DataFrame(setup_scraped_reviews())
 ], ignore_index=True)
 
-def determine_sentiment(text):
+def get_text_blob_sentiment(text):
     analysis = TextBlob(text)
-    if analysis.sentiment.polarity > 0:
+    if analysis.sentiment.polarity > 0.05:
         return Sentiment.POSITIVE.name
-    elif analysis.sentiment.polarity < 0:
+    elif analysis.sentiment.polarity < -0.05:
         return Sentiment.NEGATIVE.name
     else:
         return Sentiment.NEUTRAL.name 
-all_reviews['Sentiment'] = all_reviews['Review'].apply(determine_sentiment)
+all_reviews['TextBlob_Sentiment'] = all_reviews['Review'].apply(get_text_blob_sentiment)
+
+def get_vader_sentiment(text):
+    nltk.download('vader_lexicon')
+    sia = SentimentIntensityAnalyzer()
+    scores = sia.polarity_scores(text)
+    compound_score = scores['compound']
+    if compound_score >= 0.05:
+        return Sentiment.POSITIVE.name
+    elif compound_score <= -0.05:
+         return Sentiment.NEGATIVE.name
+    else:
+         return Sentiment.NEUTRAL.name 
+all_reviews['VADER_Sentiment'] = all_reviews['Review'].apply(get_vader_sentiment)
     
 engine = create_engine(f'mysql+pymysql://root:BigData@127.0.0.1:3306/bigdataengineer')
 all_reviews.to_sql('all_reviews', con=engine, if_exists='replace', index=False)
