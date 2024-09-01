9a7553ac31a2946e612926eacb80c135853c47b4
Wumpie7 <chrisjaver@gmail.com>
2024-09-01 22:06:31 +0200

    Added basic implementation of dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient 
 import pandas as pd
+import streamlit as st
 
 client = MongoClient("localhost", 27017)
 db = client['Big']
@@ -7,4 +8,9 @@ collection = db['Data']
 
 df = pd.DataFrame(list(collection.find().limit(10000)))
 
-print(df)
+st.title("Hotel Reviews Data Dashboard")
+st.write("Big Data Engineer Dashboard.")
+
+st.sidebar.header("Select columns to display")
+selected_columns = st.sidebar.multiselect("", df.columns.tolist(), default=df.columns.tolist())
+st.dataframe(df[selected_columns])
