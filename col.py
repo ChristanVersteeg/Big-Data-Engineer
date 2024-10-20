5df849214da379642f3bc511161ff5891e1f0e78
Wumpie7 <chrisjaver@gmail.com>
2024-10-20 19:27:39 +0200

    Added dashboard column management

---
+++ col.py
@@ -0,0 +1,37 @@
+ID = '_id'
+HOTEL_ADDRESS = 'Hotel_Address'
+ADDITIONAL_NUMBER_OF_SCORING = 'Additional_Number_of_Scoring'
+REVIEW_DATE = 'Review_Date'
+AVERAGE_SCORE = 'Average_Score'
+HOTEL_NAME = 'Hotel_Name'
+REVIEWER_NATIONALITY = 'Reviewer_Nationality'
+NEGATIVE_REVIEW = 'Negative_Review'
+REVIEW_TOTAL_NEGATIVE_WORD_COUNTS = 'Review_Total_Negative_Word_Counts'
+TOTAL_NUMBER_OF_REVIEWS = 'Total_Number_of_Reviews'
+POSITIVE_REVIEW = 'Positive_Review'
+REVIEW_TOTAL_POSITIVE_WORD_COUNTS = 'Review_Total_Positive_Word_Counts'
+TOTAL_NUMBER_OF_REVIEWS_REVIEWER_HAS_GIVEN = 'Total_Number_of_Reviews_Reviewer_Has_Given'
+REVIEWER_SCORE = 'Reviewer_Score'
+TAGS = 'Tags'
+DAYS_SINCE_REVIEW = 'days_since_review'
+LAT = 'lat'
+LNG = 'lng'
+
+included_columns = [
+    #ID, 
+    #HOTEL_ADDRESS, 
+    #ADDITIONAL_NUMBER_OF_SCORING, 
+    REVIEW_DATE,
+    #AVERAGE_SCORE, 
+    HOTEL_NAME, 
+    #REVIEWER_NATIONALITY, 
+    NEGATIVE_REVIEW,
+    #REVIEW_TOTAL_NEGATIVE_WORD_COUNTS, 
+    #TOTAL_NUMBER_OF_REVIEWS, 
+    POSITIVE_REVIEW,
+    #REVIEW_TOTAL_POSITIVE_WORD_COUNTS,
+    #TOTAL_NUMBER_OF_REVIEWS_REVIEWER_HAS_GIVEN,
+    REVIEWER_SCORE, 
+    #TAGS, 
+    #DAYS_SINCE_REVIEW
+]
--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient 
 import pandas as pd
 import streamlit as st
+import col
 
 client = MongoClient("localhost", 27017)
 db = client['Big']
@@ -12,5 +13,5 @@ st.title("Hotel Reviews Data Dashboard")
 st.write("Big Data Engineer Dashboard.")
 
 st.sidebar.header("Select columns to display")
-selected_columns = st.sidebar.multiselect("", df.columns.tolist(), default=df.columns.tolist())
+selected_columns = st.sidebar.multiselect("", df.columns.tolist(), default=col.included_columns)
 st.dataframe(df[selected_columns])
